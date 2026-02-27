db87de54a94f61e69088a17253c34dad709f0b18
wyrobowa <aniawyroba@gmail.com>
2026-02-27 13:51:17 +0100

    feat: update project name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/events-list/',
   esbuild: {
     loader: 'tsx',
     include: /src\/.*\.tsx?$/,
